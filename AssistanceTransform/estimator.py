185be4d77537b7b83c384c88abaddc009dcc039e
Casper Smet <casper.smet@student.hu.nl>
2020-09-18 14:40:23 +0200

    [FLUID-27] Area estimator

--- AssistanceTransform/estimator.py
+++ AssistanceTransform/estimator.py
@@ -3,16 +3,25 @@ from typing import List
 
 import numpy as np
 
+from AssistanceTransform.exceptions import DimensionError
+
 
 def area(poly: np.ndarray) -> float:
-    """Estimates the area of a polygon.
+    """Estimates the area of a polygon using the .
 
     :param poly: Polygon
     :type poly: Numpy array
-    :return: Area of polygon in m
+    :return: Area of polygon
     :rtype: float
     """
-    pass
+    if not isinstance(poly, np.ndarray):
+        raise TypeError(f"Expected poly to be `np.ndarray`, not {type(poly)}")
+    if len(poly.shape) != 2:
+        raise DimensionError(f"Expected dimension (n, 2), not {poly.shape}")
+    if poly.shape[1] != 2:
+        raise DimensionError(f"Expected dimension (n, 2), not {poly.shape}")
+    x, y = poly.T
+    return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
 
 
 def estimate_release_rate(polys: List[np.ndarray], time_indices: List[dt.datetime]) -> float:
@@ -25,4 +34,5 @@ def estimate_release_rate(polys: List[np.ndarray], time_indices: List[dt.datetim
     :return: m^2 s^-1
     :rtype: float
     """
-    pass
+    raise NotImplementedError(
+        "Function `estimate_release_rate` in module `estimator.py` has not yet been implemented")
