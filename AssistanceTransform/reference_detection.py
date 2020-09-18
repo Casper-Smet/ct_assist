117d05e306a0f6555bc6f929094980a2bc6aae56
Casper Smet <casper.smet@student.hu.nl>
2020-09-18 15:49:19 +0200

    [FLUID-24] Empty code for reference_detection

--- AssistanceTransform/reference_detection.py
+++ AssistanceTransform/reference_detection.py
@@ -1,1 +1,28 @@
-raise NotImplementedError("reference_detection.py has not yet been implemented")
+from detectron2.engine import DefaultPredictor
+import numpy as np
+
+
+def load_model(classes: list, model_dir: str = None, model_name: str = None) -> DefaultPredictor:
+    """Loads Detectron2 model
+
+    :param classes: Classes to detect for
+    :type classes: list
+    :param model_dir: Directory containig model, defaults to None
+    :type model_dir: str, optional
+    :param model_name: Model file name, defaults to None
+    :type model_name: str, optional
+    :return: Detectron2 predictor for objects
+    :rtype: DefaultPredictor
+    """
+    raise NotImplementedError("Function `load_model` in `reference_detection.py` has not yet been implemented")
+
+
+def extract_reference(objects: np.ndarray) -> np.ndarray:
+    """Extracts reference objects' feet and heads
+
+    :param objects: Segmentation masks
+    :type objects: np.ndarray
+    :return: Reference feet, heads, average height, STD height
+    :rtype: np.ndarray
+    """
+    raise NotImplementedError("Function `load_model` in `reference_detection.py` has not yet been implemented")
--- AssistanceTransform/tests/test_reference_detection.py
+++ AssistanceTransform/tests/test_reference_detection.py
@@ -1,2 +1,11 @@
-def test_unimplemented():
-    from AssistanceTransform import reference_detection
+from AssistanceTransform import reference_detection
+
+
+def test_load_model():
+    """Loads Detectron2 model"""
+    raise NotImplementedError("Function `load_model` in `reference_detection.py` has not yet been implemented")
+
+
+def test_extract_reference():
+    """Extracts reference objects' feet and heads"""
+    raise NotImplementedError("Function `load_model` in `reference_detection.py` has not yet been implemented")
