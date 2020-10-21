a6dbfec8c7279a2a6dabb516eadde7e64dbad887
Casper Smet <casper.smet@student.hu.nl>
2020-10-21 14:42:35 +0200

    Tested load_model

--- AssistanceTransform/tests/test_reference_detection.py
+++ AssistanceTransform/tests/test_reference_detection.py
@@ -1,9 +1,16 @@
-from AssistanceTransform import reference_detection
+from AssistanceTransform import reference_detection as rf
+
+from detectron2.engine import DefaultPredictor
+from detectron2.config.config import CfgNode
 
 
 def test_load_model():
     """Loads Detectron2 model"""
-    raise NotImplementedError("Function `load_model` in `reference_detection.py` has not yet been implemented")
+    assert isinstance(rf.load_model(), DefaultPredictor), "rf.load_model() != DefaultPredictor"
+    dp, cfg = rf.load_model(return_cfg=True)
+    assert isinstance(dp, DefaultPredictor), "rf.load_model(return_cfg=True), != (DefaultPredictor, ...)"
+    assert isinstance(cfg, CfgNode), "rf.load_model(return_cfg=True), != (... CfgNode)"
+    assert cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST == 0.7
 
 
 def test_extract_reference():
