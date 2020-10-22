15452708676382882fc042943bfe260bb8315210
Casper Smet <casper.smet@student.hu.nl>
2020-10-22 14:53:49 +0200

    extract reference

--- AssistanceTransform/reference_detection.py
+++ AssistanceTransform/reference_detection.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+from typing import Tuple, List
+from warnings import warn
 
 import detectron2
 import numpy as np
@@ -9,6 +11,10 @@ from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.engine import DefaultPredictor
 from detectron2.structures import Instances
 from detectron2.utils.visualizer import Visualizer
+
+const_height_dict: dict = {"truck": (3, 1),
+                           "person": (1.741, 0.05),
+                           "car": (1.425, 0.0247)}
 
 
 def load_model(model_url: str = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml", threshold: float = 0.7,
@@ -35,7 +41,7 @@ def load_model(model_url: str = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x
         return predictor
 
 
-def instances_to_dict(preds: dict, thing_classes: dict) -> defaultdict:
+def instances_to_dict(preds: dict, thing_classes: list) -> defaultdict:
     """Take Detectron2.engine.DefaultPredictor output, and turns it into an easily parsable dictionary.
 
     Have a cfg ready? Use: detectron2.data.MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).get("thing_classes"))
@@ -43,8 +49,8 @@ def instances_to_dict(preds: dict, thing_classes: dict) -> defaultdict:
 
     :param preds: dict to Instances object containing segmentation masks, output from defaultpredictor
     :type preds: dict
-    :param thing_classes: Dictionary mapping integers to string
-    :type thing_classes: dict
+    :param thing_classes: List mapping integers to string
+    :type thing_classes: list
     :return: object-name : [masks]
     :rtype: defaultdict
     """
@@ -88,13 +94,31 @@ def get_heads_feet(mask: torch.tensor, step_size=5, min_size=0.9):
     # Apply step size
     return reference[:, 0::step_size]
 
-# def extract_reference(objects: np.ndarray) -> np.ndarray:
-#     """Extracts reference objects' feet and heads
 
-#     :param objects: Segmentation masks
-#     :type objects: np.ndarray
-#     :return: Reference feet, heads, average height, STD height
-#     :rtype: np.ndarray
-#     """
-#     raise NotImplementedError(
-#         "Function `load_model` in `reference_detection.py` has not yet been implemented")
+def extract_reference(objects: dict, step_size: int = 10, min_size: float = 0.9,
+                      height_dict: dict = const_height_dict) -> List[Tuple[np.ndarray, np.ndarray, float]]:
+    """Extracts references from dictionary filled with predictions.
+
+    See instances_to_dict for objects' format. The output is based on the output for get_heads_feet.
+
+    :param objects: key : [mask]
+    :type objects: dict
+    :param step_size: How many pixels to skip, defaults to 10
+    :type step_size: int, optional
+    :param min_size: Minimum size relative to maximum distance between heads and feet, defaults to 0.9
+    :type min_size: float, optional
+    :return: [(reference, height, STD)]
+    :rtype: List[Tuple[np.ndarray, np.ndarray, float]]
+    """
+    args = []
+    for key, masks in objects.items():
+        try:
+            height, STD = height_dict.get(key)
+        except TypeError:
+            print(UserWarning(f"Key `{key}` not found in `height_dict`. Skipped this field."))
+            continue
+        refs = [get_heads_feet(mask, step_size=step_size,
+                               min_size=min_size) for mask in masks]
+        refs = np.concatenate(refs, axis=1)
+        args.append((refs, height, STD))
+    return args
