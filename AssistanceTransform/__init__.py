0a0d3b3f87ea6723c2e7f7ac6be037e49832ae41
Casper Smet <casper.smet@student.hu.nl>
2020-11-04 15:00:44 +0100

    Removed estimator from __init__.py

--- AssistanceTransform/__init__.py
+++ AssistanceTransform/__init__.py
@@ -5,11 +5,8 @@ Python package for finding reference objects (head-feet pairs) from images, and 
 
 This package was made for TNO and EU-project ASSISTANCE.
 
-Estimator contains functions calculating the area of a polygon, and the "release rate", the rate at which the size of a polygon increases over time.
-
 Transform contains functions for fitting CameraTransform's camera, extracting exif data from images, and estimating sensor size based.
 """
 
-from . import estimator
 from . import transform
 from . import reference_detection
