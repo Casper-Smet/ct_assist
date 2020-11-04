73b3c3b2e9b8a414e2b7b70b65da2f24ce1c1c9d
Casper Smet <casper.smet@student.hu.nl>
2020-11-04 15:28:02 +0100

    Added linux to OS in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Windows 10 w. CUDA",
+        "Operating System :: Windows 10, Linux",
     ],
     python_requires='>=3.7.7',
 )
