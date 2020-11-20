2bc6417b0c95ac6148f537b048a1374a2b3abfd5
Casper Smet <casper.smet@student.hu.nl>
2020-11-20 15:57:09 +0100

    Version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ct_assist",
-    version="0.5",
+    version="1.0",
     author="Casper Smet",
     author_email="casper.smet@gmail.com",
     description="Automatically finding head-feet pairs for CameraTransform",
